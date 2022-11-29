ab8b30490daef15141684112f10e652a15785b1c
sammiearchie77 <chinonsosamuel29@gmail.com>
2022-11-29 08:05:02 -0800

    Changed files to executable files

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for n in range(97, 123):
-    if n != 101 and n != 113:
-        print("{:c}".format(chr(n)), end=' '))
+for character in range(97, 123):
+    if (character != 101 and character != 113):
+        print("{:c}".format(character), end='')
