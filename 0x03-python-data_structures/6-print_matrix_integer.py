7d329ef3af6c95c880d2376559d1c07c70606f83
sammiearchie77 <chinonsosamuel29@gmail.com>
2022-12-04 07:59:35 -0800

    6-print_matrix_integer.py indentation error

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,12 +2,12 @@
 
 def print_matrix_integer(matrix=[[]]):
     if len(matrix[0]) != 0:
-            rows = len(matrix)
-            cols = len(matrix[0])
-            for r in range(rows):
-                for c in range(cols):
-                    if c == cols - 1:
-                        print('{:d}'.format(matrix[r][c]))
-                    else:
-                        print('{:d}'.format(matrix[r][c]), end=' ')
+        rows = len(matrix)
+        cols = len(matrix[0])
+        for r in range(rows):
+            for c in range(cols):
+                if c == cols - 1:
+                    print('{:d}'.format(matrix[r][c]))
+                else:
+                    print('{:d}'.format(matrix[r][c]), end=' ')
     else: print()
