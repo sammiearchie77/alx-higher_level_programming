59d80d51b7ed01f69624edf4a7965441f62aa7f9
sammiearchie77 <chinonsosamuel29@gmail.com>
2022-12-04 08:29:39 -0800

    Add tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,19 +1,11 @@
+
+
 #!/usr/bin/python3
-
-def standardize_tuple(tup):
-    tup_len = len(tup)
-    if tup_len == 0:
-        return (0, 0)
-    elif tup_len == 1:
-        return (tup[0], 0)
-    else:
-        return (tup[0], tup[1])
-
 def add_tuple(tuple_a=(), tuple_b=()):
-    sum1 = 0
-    sum2 = 0
-    tup_a_st = standardize_tuple(tuple_a)
-    tup_b_st = standardize_tuple(tuple_b)
-    sum1 = tup_a_st[0] + tup_b_st[0]
-    sum2 = tup_a_st[1] + tup_b_st[1]
-    return (sum1, sum2)
+    a, b = 0, 0
+    for tupl in (tuple_a, tuple_b):
+        if len(tupl) > 0:
+            a += tupl[0]
+        if len(tupl) > 1:
+            b += tupl[1]
+    return a, b
