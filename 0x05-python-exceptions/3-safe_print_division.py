1630eb03a92417b2d1653825e83c7bf434bc3eee
sammiearchie77 <chinonsosamuel29@gmail.com>
2022-12-19 06:54:26 -0800

    Mandatory task

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,12 +3,11 @@
 def safe_print_division(a,b):
     quotient = 0
     try:
-        quotient = a / b
+        quotient = a/b
     except ZeroDivisionError:
         quotient = None
     except TypeError:
         quotient = None
     finally:
         print('Inside result: {}'.format(quotient))
-
     return quotient
