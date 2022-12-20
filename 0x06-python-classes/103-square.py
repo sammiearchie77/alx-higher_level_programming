3d4604b012bfe60ebda46ea235b32c3403985524
sammiearchie77 <chinonsosamuel29@gmail.com>
2022-12-20 14:35:45 -0800

    W291 trailing whitespace fix

--- 0x06-python-classes/103-square.py
+++ 0x06-python-classes/103-square.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-"""writing a docstring"""
 import math
-
 
 class MagicClass:
     """set up the magic"""
-
-    def __init__(self, radius=0):
-        """ writing another docstring """
+    def __init__(self, radius):
+        """ Constructor """
         self.__radius = 0
-        if type(radius) is not int and type(radius) is not float:
+        if not isinstance(radius, int) and not isinstance(radius, float):
             raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area(self):
-        """again with the docstring"""
+        """ Find the area """
+
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
-        """such docstring"""
+        """ get circumference """
+
         return 2 * math.pi * self.__radius
+
