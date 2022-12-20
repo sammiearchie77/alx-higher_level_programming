7b518b646e6b784b65fd5977c2d6f63194c54382
sammiearchie77 <chinonsosamuel29@gmail.com>
2022-12-20 14:45:08 -0800

    Corrected filename error

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
+"""writing a docstring"""
 import math
+
 
 class MagicClass:
     """set up the magic"""
-    def __init__(self, radius):
-        """ Constructor """
+
+    def __init__(self, radius=0):
+        """ writing another docstring """
         self.__radius = 0
-        if not isinstance(radius, int) and not isinstance(radius, float):
+        if type(radius) is not int and type(radius) is not float:
             raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area(self):
-        """ Find the area """
-
+        """again with the docstring"""
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
-        """ get circumference """
-
+        """such docstring"""
         return 2 * math.pi * self.__radius
-
