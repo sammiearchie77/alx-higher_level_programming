2a8e9b117b7bcccb5833e06e76c318f0db1b76e5
sammiearchie77 <chinonsosamuel29@gmail.com>
2023-05-13 12:12:59 +0100

    Testing 6-max_integer.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -2,7 +2,7 @@
 """Unittest for max_integer([..])
 """
 import unittest
-#max_integer = __import__('6-max_integer').max_integer
+max_integer = __import__('6-max_integer').max_integer
 
 
 def max_integer(list=[]):
